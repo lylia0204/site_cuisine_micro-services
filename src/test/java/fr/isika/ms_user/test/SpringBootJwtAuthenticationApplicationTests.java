ca27e9a65d949cf3970cc190c984db73de09722b
lilya <lilya.belaid16@gmail.com>
2020-07-19 15:34:34 +0200

    test

--- src/test/java/fr/isika/ms_user/test/SpringBootJwtAuthenticationApplicationTests.java
+++ src/test/java/fr/isika/ms_user/test/SpringBootJwtAuthenticationApplicationTests.java
@@ -1,16 +1,16 @@
-package fr.isika.ms_user.test;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SpringBootJwtAuthenticationApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package fr.isika.ms_user.test;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class SpringBootJwtAuthenticationApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
