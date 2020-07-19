29a5230e10bc80310216a0d82a0719f33d5ff0e2
lilya <lilya.belaid16@gmail.com>
2020-07-19 13:08:32 +0200

    ajout du buildspec et appspec

---
+++ scripts/after_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mv /opt/ms-user/ms-user /etc/init.d/
+chmod +x /etc/init.d/ms-user
---
+++ scripts/before_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#sleep 60
+mkdir /opt/ms-user
+rm -rf /opt/ms-user/SpringBootJwtAuthentication-0.0.1*
+rm -rf /etc/init.d/ms-user
+
+
